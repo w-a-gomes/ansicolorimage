bded40254589a1e01cfb00fea9c19e5c0f0bef8a
w-a-gomes <wildenalisson@outlook.com>
2022-10-29 00:36:55 -0300

    examples

--- examples/main.py
+++ examples/main.py
@@ -6,32 +6,80 @@ base_dir = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(base_dir.as_posix())
 from src.ansicolorimage import ImageColorMap
 
+
 # /usr/share/pixmaps/neon.png
-img = ImageColorMap(
-    url_image=base_dir.as_posix() + '/examples/data/neon.png',
-    contrast=1.3,
-    brightness=0.85)
-for item in img.ascii_lines:
-    print(item)
+def neon_ex():
+    img = ImageColorMap(
+        url_image=base_dir.as_posix() + '/examples/data/neon.png',
+        contrast=1.3,
+        brightness=0.85)
+    for item in img.ascii_lines:
+        print(item)
 
-img = ImageColorMap(
-    url_image=base_dir.as_posix() + '/examples/data/python.png')
-for item in img.ascii_lines:
-    print(item)
 
-img = ImageColorMap(
-    url_image=base_dir.as_posix() + '/examples/data/debian.png',
-    contrast=1.2,
-    brightness=0.95,
-    ascii_map=[' ', ' ', '*', '&'] + ['#'] * 20)
-for item in img.ascii_lines:
-    print(item)
+def python_ex():
+    img = ImageColorMap(
+        url_image=base_dir.as_posix() + '/examples/data/python.png')
+    for item in img.ascii_lines:
+        print(item)
 
-img = ImageColorMap(
-    url_image=base_dir.as_posix() + '/examples/data/poe.jpg',
-    height=25,
-    width=50,
-    show_background_color=True,
-    hide_foreground_character=True)
-for item in img.ascii_lines:
-    print(item)
+
+def debian_ex():
+    img = ImageColorMap(
+        url_image=base_dir.as_posix() + '/examples/data/debian.png',
+        contrast=1.2,
+        brightness=0.95,
+        ascii_map=[' ', ' ', '*', '&'] + ['#'] * 20)
+    for item in img.ascii_lines:
+        print(item)
+
+
+def poe_ex():
+    poem = """
+    
+    The Raven
+    
+    ...
+     Then this ebony bird beguiling my sad fancy into smiling,
+    By the grave and stern decorum of the countenance it wore,
+    “Though thy crest be shorn and shaven, thou,” I said, “art sure no craven,
+    Ghastly grim and ancient Raven wandering from the Nightly shore—
+    Tell me what thy lordly name is on the Night’s Plutonian shore!”
+                Quoth the Raven “Nevermore.”
+    ...
+    But the Raven still beguiling all my fancy into smiling,
+    Straight I wheeled a cushioned seat in front of bird, and bust and door;
+        Then, upon the velvet sinking, I betook myself to linking
+        Fancy unto fancy, thinking what this ominous bird of yore—
+    What this grim, ungainly, ghastly, gaunt, and ominous bird of yore
+                Meant in croaking “Nevermore.”
+    ...
+    
+    
+    (By Edgar Allan Poe)
+    
+    """
+    img = ImageColorMap(
+        url_image=base_dir.as_posix() + '/examples/data/poe.jpg',
+        height=25,
+        width=50,
+        show_background_color=True,
+        hide_foreground_character=True)
+    for txt, logo in zip(poem.split('\n'), img.ascii_lines):
+        print(logo, txt)
+
+
+def kali_ex():
+    img = ImageColorMap(
+        url_image=base_dir.as_posix() + '/examples/data/kali.jpg',
+        height=30,
+        width=60,
+        contrast=1.2,
+        brightness=0.95,
+        ascii_map=['0', '0'] + ['1'] * 25)
+    for item in img.ascii_lines:
+        print(item)
+
+
+if __name__ == '__main__':
+    python_ex()
